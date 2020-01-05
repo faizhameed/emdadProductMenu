1e2cb2f73f1986f561eafd75c3f35910ac4d0f07
faizhameed <faizftb@gmail.com>
2020-01-05 22:51:04 +0530

    added rubber sheet categories

--- src/component/rubber-sheet/rubber-sheet.jsx
+++ src/component/rubber-sheet/rubber-sheet.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const RubberSheet = () => {
-  return <div>coomgin soon</div>;
+  return (
+    <div>
+      NR Rubber Sheet NBR Rubber Sheet SBR Rubber Sheet Neoprene Rubber Sheet
+      EPDM Rubber Sheet Silicone Rubber Sheet Hypalon Rubber Sheet Latex Rubber
+      Sheet Butyl Rubber Sheet Functional Rubber Sheet
+    </div>
+  );
 };
 
 export default RubberSheet;
