784f30490d33b2166ae136402e3b61a7f838c90f
faizhameed <faizftb@gmail.com>
2020-01-14 22:03:51 +0530

    some more changes to be done

---
+++ src/component/data/product-data.js
@@ -0,0 +1,283 @@
+const productData ={
+  "Packing and seals":[
+
+    {"Graphite packing":{
+            
+    "Description": "Created from a unique braid of low sulphur expanded graphite yarns with additional reinforcement from either cotton or glass fiber, this graphite packing showcases the chemical resistance, high elasticity, thermal conductivity, and low friction factor that makes it perfect for your needs.",
+    "Uses": "Based on the above characteristics, graphite packing is ideal for a range of uses in a plant, including but not limited to pumps, mixers, expansion joints and high temperature agitators."
+    }},
+   { "Carbon Fiber Packing":{
+
+
+     "Description": "created from a braid of custom high-grade carbon fiber with additional graphite impregnation, this carbon fiber packing is self-lubricated and densely packed to prevent leakage.",
+     "Uses": "based on the above characteristics, carbon fiber packing is ideal for a range of uses in a plant, including but not limited to anti-extrusion rings, pumps, and valves, all functioning in high temperature and pressure situations, as well as in the provision of perfect sealing in dry running equipment."
+   }
+            
+            
+    },
+    
+    
+   { "Asbestos Packing":
+            
+    {"Description": "created from a braid of the highest quality asbestos yarns, asbestos packing provides strength and flexibility in a range of serving temperatures.",
+    "Uses": "based on the above characteristics, graphite packing is ideal for a range of uses in a plant, including but not limited to static sealing, as filler material under high temperature conditions."
+    }},
+{  "PTFE Packing":{
+
+  "Description": "created from a braid of either pure PTFE yarns or yarns with lube oil according to your specifications, PTFE Packing provides resistance to corrosion, low heat conductivity and non-sticking properties to suit your company needs.",
+  "Uses": "based on the above characteristics, PTFE packing is ideal for a range of uses in a plant, including but not limited to static sealing applications, as standard packing for most medium as well as in industries like paper mills where purity is an absolute requirement."
+}
+            
+            
+    },
+    
+    
+   { "Fibreglass Packing":{
+            
+    "Description": "created from a combination of organic and inorganic fibers, Fiberglass Packing has the high strength, low thermal conductivity and fire resistance that make it such an attractive alternative to asbestos.",
+    "Uses": "based on the above characteristics, Fiberglass packing is ideal for a range of uses in a plant, including but not limited to heat insulation, and fire prevention in chemically neutral media."
+    }}
+      ,
+  { "Aramid Fiber Packing":{
+     "Description": "created from a braid of Kevlar yarn, this high quality material has been uniformly impregnated with PTFE to confer properties like chemical resistance, elasticity and wear resistance. It may also be impregnated with a silicone-based lubricant to withstand wear.",
+     "Uses": "based on the above characteristics, Aramid Fiber packing is ideal for a range of uses in a plant, including but not limited to working with abrasive materials and media, and in combination with other materials as an anti-extrusion ring."
+
+   }},
+            
+    
+  {  "Ceramic Fiber Packing":{
+
+      "Description": "created from square braided ceramic fiber yarns, ceramic f6riber Packing is manufactured from high quality alumina-silica materials that possess resistance to almost all chemically corrosive agents, and high temperature stability with low heat storage.",
+      "Uses": "based on the above characteristics, ceramic fiber packing is ideal for a range of uses in a plant, including but not limited to static seal packing in areas where thermal resistance is preferred, and in high temperature valves."
+    }
+        }    
+    ,
+   {" Cotton Fiber Packing":{
+            
+    "Description": "created from a braid of cotton fibre yarns, cotton fibre packing can be impregnated with either yellow grease or graphite to be flexible and easy to use.",
+    "Uses": "based on the above characteristics, cotton fibre packing is ideal for a range of uses in a plant, including but not limited to use as standard packing in a variety of applications, as well as in reciprocating valves."
+    }},
+   {"Ramie Fiber Packing":{
+
+     "Description": "created from a braid of ramie fibres that could either be impregnated with PTFE, lube oil or with a silicon core, ramie fibre packing can be used in a variety of delicate applications as a result of its potential for vibration absorption.",
+     "Uses": "based on the above characteristics, ramie fibre packing is ideal for a range of uses in a plant, including but not limited to use as filters or pumps in industries where abrasive media are common."
+   }}
+            
+  ]
+  ,
+    
+  "Gasket":[
+
+    {"Rubber Gasket":{
+            
+    "Description": "made from all-purpose rubber, our rubber gaskets are prized for their corrosion resistance, resilience and tensile strength."
+    ,"Uses": "based on the above characteristics, rubber gaskets ideal for a range of uses in a plant, including but not limited to insulation for sensitive materials and in areas where tensile strength is of utmost importance."
+    }},
+    {"Ceramic Gasket":{
+
+      "Description": "created from ceramic fibre paper, our ceramic gasket is affordable, lightweight and confers excellent thermal resistance."
+      ,"Uses": "based on the above characteristics, our ceramic gasket is ideal for a range of uses in a plant, including but not limited to heat sealing in low-pressure environments, and as thermally resistant insulation in sensitive equipment."
+    }},
+            
+    
+   { "Spiral Wound Gasket":{
+
+      "Description": "created from a metal hoop and non-metal filler, our spiral wound gasket is ideal for sealing areas with inconsistent temperature and pressure."
+      ,"Uses": "based on the above characteristics, graphite packing is ideal for a range of uses in a plant, including but not limited to as a sealing element in condensing towers and pumps."
+    }},
+
+            
+    
+   { "Metal Jacketed Gasket":{
+      "Description": "created from filler material enclosed by a metal jacket, our metal jacketed gasket is ideal for use when you expect to encounter high temperature and pressure."
+      ,"Uses": "based on the above characteristics, our metal jacketed gasket is ideal for a range of uses in a plant, including but not limited to engines and low-pressure compressors."
+
+    }},
+
+            
+    
+    {"Corrugated Metal Gasket":{
+      
+"Description": "created from corrugated metal with either single or double layers, our corrugated metal gasket is ideal for situations where you expect high temperatures and you need significant mechanical strength."
+,"Uses": "based on the above characteristics, the corrugated metal gasket is ideal for a range of uses in a plant, including but not limited to flanges, and in gas pipes."
+
+    }},
+    {
+    "PTFE Gasket":{
+      
+"Description": "created from PTFE resin, our PTFE gasket has one of the best resistance to chemicals on the planet as well as excellent electrical insulation. It could be also combined with glass fibre."
+,"Uses": "based on the above characteristics, our PTFE gasket is ideal for a range of uses in a plant, including but not limited to use as standard sealing material."
+
+    }},
+   { 
+   " Machined Metal Gasket":{
+     
+"Description": "created from solid metal., our machined metal gasket comes in handy when you expect high pressure and temperature conditions."
+,"Uses": "based on the above characteristics, machined metal gaskets are ideal for a range of uses in a plant, including but not limited to flanges and in high-pressure valves."
+
+   }},
+    
+   {" Flat Metal Gasket":{
+     
+"Description": "Created from solid, flat metal, our flat metal gasket should be used when you require tight sealing even in high-pressure environments. It also has a significant amount of temperature and corrosion resistance."
+,"Uses": "based on the above characteristics, the flat metal gasket is ideal for a range of uses in a plant, including but not limited to hydraulic presses and ammonia fittings."
+
+   }
+    },{
+    "Graphite Gasket":{
+
+      "Description": "Created from homogenous graphite, our graphite gasket could either be reinforced with mesh or tanged metal. This gives it extra temperature resistance as well as the ability to resist corrosion."
+      ,"Uses": "based on the above characteristics, the graphite gasket is ideal for a range of uses in a plant, including but not limited to piping and valves in offshore operations."
+    }
+      },      
+    
+    {"EZ-Seal Gasket":{
+
+      "Description": "Created from a unique design that confers supreme chemical compatibility and great cold-flow resistance, our EZ-seal gasket is extremely stress-resistant."
+      ,"Uses": "based on the above characteristics, the EZ-seal gasket is ideal for a range of uses in a plant, including but not limited to flanges in areas where you require low bolt loads."
+    }}
+            
+  ],
+
+  
+  
+  "Sheets":[
+    {"Graphite Sheet":{
+
+      "Description": "Created from the purest natural graphite, our graphite sheet is made without any chemical additives to give you material with unparalleled compressibility and chemical resistance."
+      ,"Uses": "based on the above characteristics, our graphite sheet is ideal for a range of uses in a plant, including but not limited to high turnover areas with little maintenance requirement, and as a replacement for asbestos."
+    }},
+            
+    
+    {"Rubber Sheet":{
+
+      "Description": "Created from natural rubber for its chemical and temperature resistant qualities, our rubber sheet can be manufactured to meet your particular specifications."
+      ,"Uses": "based on the above characteristics, the rubber sheet is ideal for a range of uses in a plant, including but not limited to as gaskets, seals or sealing material."
+    }
+            
+    },
+   { "NR Rubber Sheet":{
+      "Description": "created from natural rubber with a host of additives, our NR rubber sheet has properties that can be adjusted to meet specific requirements."
+      ,"Uses": "based on the above characteristics, our NR rubber sheet is ideal for a range of uses in a plant, including but not limited to buffering rings, washers, and buffering rings."
+
+    }}
+        ,    
+    
+    {"NBR Rubber Sheet":{
+            
+    "Description": "Created from nitrile rubber due to its resistance to mineral oils and acids, our NBR rubber sheet also has phenomenal tensile strength and resilience"
+    ,"Uses": "based on the above characteristics, the NBR rubber sheet is ideal for a range of uses in a plant, including but not limited to as seals in gasoline and oil environments."
+   } },
+
+    {"SBR Rubber Sheet":{
+            
+    "Description": "Created from styrene-butadiene rubber as a result of its extreme resistance to wear and tear, our STR rubber sheet is a cheaper alternative to natural rubber. Unlike NBR, however, it has little chemical resistance."
+    ,"Uses": "based on the above characteristics, SBR rubber sheet is ideal for a range of uses in a plant, including but not limited to heating and plumbing, where chemical and oil resistance isn’t a requirement."
+    }},
+    {"Neoprene Rubber Sheet":{
+
+      "Description": "created from neoprene rubber as a result of its ability to stand up to wear and tear, our neoprene rubber sheet is an all-purpose, water and ozone resistant material."
+      ,"Uses": "based on the above characteristics, neoprene rubber is ideal for a range of uses in a plant, including but not limited to electronic and high-temperature environments."
+    }},
+            
+    
+    {"EPDM Rubber Sheet":{
+            
+    "Description": "Created to be an all-purpose material with great heat and chemical resistance, but poor oil resistance, our EPDM rubber sheet can be used where environmental conditions are poor."
+    ,"Uses": "based on the above characteristics, EPDM rubber sheet is ideal for a range of uses in a plant, including but not limited to exterior covering, and as seals in areas where oil resistance is not a requirement."
+    }},
+    {"Silicone Rubber Sheet":{
+            
+    "Description": "created from silicone, these sheets evince a range of properties including chemical resistance, resistance to wear, and lightweight."
+    ,"Uses": "based on the above characteristics, the silicone rubber sheet is ideal for a range of uses in a plant, including but not limited to as seals in areas that require oil resistance and in exterior environments."
+    }},
+    {"Hypalon Rubber Sheet":{
+            
+    "Description": "created from chloro-sulfonated polyethene, our Hypalon rubber sheet showcases tool ozone resistance and phenomenal weather resistance. It could also function as a replacement for neoprene in certain settings."
+    ,"Uses": "based on the above characteristics, the Hypalon rubber sheet is ideal for a range of uses in a plant, including but not limited to high-temperature settings where natural rubber is unavailable, and as seals in extreme external environments."
+    }},
+    {"Latex Rubber Sheet":{
+            
+    "Description": "created from the natural secretion from the rubber tree, our pure latex rubber sheets possess excellent strength and elasticity."
+    ,"Uses": "based on the above characteristics, our natural latex rubber sheet is ideal for a range of uses in a plant, including but not limited to seals and diaphragms as well as a protective material."
+    }},
+    Butyl Rubber Sheet
+            
+    "Description": "created from Isobutylene-isoprene, our butyl rubber sheet is gas and wear-resistant, and is practically impermeable to air."
+    ,"Uses": "based on the above characteristics, out butyl rubber sheet is ideal for a range of uses in a plant, including but not limited to as gaskets and seals in low permeable environments."
+    
+    Functional Rubber Sheet
+            
+    "Description": "These functional rubber sheets range from anti-static rubber sheets to acid-resistant sheets, and as the name implies, can be manufactured to suit a range of requirements."
+    ,"Uses": "based on the above characteristics, functional rubber sheets are ideal for a range of uses, including but not limited to situations requiring high temperature and chemical resistance, or situations with the custom requirement."
+    
+    Compressed Asbestos Rubber Sheet
+            
+    "Description": "created from asbestos fibres, natural rubber and filler, our compressed asbestos rubber sheet combines the best of rubber and asbestos to suit your needs."
+    ,"Uses": "based on the above characteristics, the compressed asbestos rubber sheet is ideal for a range of uses in a plant, including but not limited to seals in high-performance environments, and in areas where cost is a consideration."
+    
+    Compressed Sheet Asbestos-Free
+            
+    "Description": "created from a non-asbestos fibre, our compressed asbestos-free sheet contains latex binder and filling material that gives it unique heat and acid resistance properties."
+    ,"Uses": "based on the above characteristics, graphite packing is ideal for a range of uses in a plant, including but not limited to chemical gaskets in farming and industrial machinery."
+    GS3100 Non-asbestos latex sheet
+    
+    PTFE Sheet
+            
+    "Description": "created from either pure or reinforced polytetrafluoroethylene, our PTFE sheet provides resistance to a wide range of chemicals as well as electrical insulation and resistance to wear and tear."
+    ,"Uses": "based on the above characteristics, PTFE sheet is ideal for a range of uses in a plant, including but not limited to sealing material and lining pads."
+    
+    Mica Sheet
+            
+    "Description": "created from either muscovite or phlogopite paper, our mica sheet was created to be flexible and heat resistant."
+    ,"Uses": "based on the above characteristics, our mica sheet is ideal for a range of uses in a plant, including but not limited to parts in metallurgy and as gasket material."
+    
+    Cork Sheet
+            
+    "Description": "created from natural granulated cork, our cork sheet was created to combine water resistance and resilience."
+    ,"Uses": "based on the above characteristics, our cork sheet is ideal for a range of uses in a plant, including but not limited to seals, and in settings that require oil resistance."
+    
+    Composite Beater Sheet
+            
+    "Description": "created from composite of asbestos fiber and inorganic filler, our composite beater sheet was made to be less dense while preserving its compressibility."
+    ,"Uses": "based on the above characteristics, the composite beater sheet is ideal for a range of uses in a plant, including but not limited to gaskets in high temperature conditions."
+      
+  ]
+  
+  PTFE
+  PTFE Sheet
+          
+  "Description": "created from polytetrafluoroethylene, PTFE is known for its chemical resistance as well as its electricity and wear resistance."
+  ,"Uses": "based on the above characteristics, PTFE is ideal for a range of uses in a plant, including but not limited to components requiring acid and alkali resistance"
+  
+  PTFE Rod/Sheet
+          
+  "Description": "created from either pure or reinforced polytetrafluoroethylene, our PTFE sheet provides resistance to a wide range of chemicals as well as electrical insulation and resistance to wear and tear."
+  ,"Uses": "based on the above characteristics, PTFE sheet is ideal for a range of uses in a plant, including but not limited to sealing material and lining pads."
+  
+  PTFE Hose
+          
+  "Description": "created either with PTFE with additives or reinforced with steel wire braiding, our PTFE hose stands up to both chemicals and wear and tear."
+  ,"Uses": "based on the above characteristics, our PTFE hose is ideal for a range of uses in a plant, including but not limited to protective covering for chemically sensitive materials."
+  
+  PTFE Film/Tape
+          
+  "Description": "created from high strength polytetrafluoroethylene, our PTFE tape is designed to be thinner for use in a number of flexible operations."
+  ,"Uses": "based on the above characteristics, PTFE is ideal for a range of uses in a plant, including but not limited to as adhesive material in areas where PVC cannot be used, and in electrical equipment."
+  
+  Expanded PTFE Tape/Cord
+          
+  "Description": "created from pure PTFE, our expanded PTFE tape retains the unique properties of the parent material in a much more manageable form"
+  ,"Uses": "based on the above characteristics, our expanded PTFE tape is ideal for a range of uses in a plant, including but not limited to sealing pipe systems and glass."
+  
+  PTFE Gasket
+          
+  "Description": "created from PTFE resin, our PTFE gasket has one of the best resistance to chemicals on the planet as well as excellent electrical insulation. It could be also combined with glass fibre."
+  ,"Uses": "based on the above characteristics, our PTFE gasket is ideal for a range of uses in a plant, including but not limited to use as standard sealing material."
+  
+  Filled PTFE Products
+          
+  "Description": "created from filled PTFE resin, out PTFE filled products combine compression strength with low thermal expansion."
+  ,"Uses": "based on the above characteristics, our PTFE filled products are ideal for a range of uses in a plant, including but not limited to valve seats and piston rings."
+  
+}
