9c4d9b80e6288173af3da5f77493bc7559fcf694
faizhameed <faizftb@gmail.com>
2020-01-15 14:49:45 +0530

    Grid added to components, all datas converted to JSON

--- src/component/product-menu.component.jsx
+++ src/component/product-menu.component.jsx
@@ -6,8 +6,10 @@ import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import RubberSheet from "./rubber-sheet/rubber-sheet";
 import useWindowSize from "../utils/window-size";
+import productData from "./data/product-data";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -84,179 +86,27 @@ export default function VerticalTabs() {
         <Tab label="PTFE" {...a11yProps(3)} />
       </Tabs>
       <React.Fragment>
-        <TabPanel value={value} index={0}>
-          <div>
-            <img src="" alt="" />
-            <h3>Graphite Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Carbon Fiber Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Asbestos Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>PTFE Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Fiberglass Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Aramid Fiber Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Ceramic Fiber Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Cotton Fiber Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Ramie Fiber Packing</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <div>
-            <img src="" alt="" />
-            <h3>Rubber Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Ceramic Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Spiral Wound Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Metal Jacketed Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Corrugated Metal Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>PTFE Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Machined Metal Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Flat Metal Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Graphite Gasket</h3>
-            <p>Graphite packings are used for....</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>EZ-Seal Gasket</h3>
-            <p>content will come soon</p>
-          </div>
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <div>
-            <img src="" alt="" />
-            <h3>Graphite Sheet</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Rubber Sheet</h3>
-            <RubberSheet />
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Compressed Asbestos Rubber Sheet</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Compressed Sheet Asbestos-Free</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>PTFE Sheet</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Mica Sheet</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Cork Sheet</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Composite Beater Sheets</h3>
-            <p>rubber</p>
-          </div>
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          <div>
-            <img src="" alt="" />
-            <h3>PTFE Sheet</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>PTFE Rod Sheet</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>PTFE Film/Tape</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Expanded PTFE Tape/Cord</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>PTFE Gasket</h3>
-            <p>rubber</p>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <h3>Filled PTFE Products</h3>
-            <p>rubber</p>
-          </div>
-        </TabPanel>
+        {Object.keys(productData).map((product, i) => {
+          return (
+            <TabPanel value={value} index={i}>
+              <Grid container spacing={2}>
+                {productData[product].map(item => {
+                  return (
+                    <Grid>
+                      <Paper className="product-item">
+                        <img src="" alt="" />
+                        <h3>{Object.keys(item)}</h3>
+                        <p>{item[Object.keys(item)[0]].Description}</p>
+                        <h4>Uses</h4>
+                        <p>{item[Object.keys(item)[0]].Uses}</p>
+                      </Paper>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </TabPanel>
+          );
+        })}
       </React.Fragment>
     </div>
   );
